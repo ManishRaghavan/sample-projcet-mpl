14a07b5510fa6d4cbc7bd5d1a3121a5a6405a2c8
ManishRR <manishrr@mplgaming.com>
2022-01-26 20:57:05 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import { Box } from "@mui/material";
+import React from "react";
 import "./App.css";
-import asset from "./asset_image.png";
+import "./styles.css";
 import MediaCard from "./Card";
 
 function App() {
   return (
     <div className="App">
       <div className="main">
-        <img className="img" src={asset} alt="asset" />
-        <h1>P2E</h1>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-          }}
-        >
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
-        </Box>
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
+        <MediaCard />
       </div>
     </div>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,39 +1,77 @@
+import React, { Suspense } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Model from "./Iphone";
+
+declare global {
+  interface Window {
+    mplAndroidView: any;
+    openingFromAndroid: any;
+  }
+}
 
 export default function MediaCard() {
-  let mplAndroidView: any;
-  const handleOpen = () => {
-    let mplAndroidView: any;
-    try {
-      mplAndroidView.performAction("OPEN_GAME");
-    } catch (er: any) {
-      console.log("anroid-error");
-      console.log(er.message);
+  React.useEffect(() => {
+    if ("undefined" !== typeof window.openingFromAndroid) {
+      alert("android is calling web");
     }
+  }, []);
+
+  const handleOpento = () => {
+    console.log("OPEN_GAME_TO");
+    if ("undefined" !== typeof window.mplAndroidView)
+      try {
+        console.log("Inside Try Block");
+        window.mplAndroidView.performAction("OPEN_GAME2");
+      } catch (er: any) {
+        console.log("anroid-error");
+        console.log(er.message);
+      }
+  };
+
+  const handleOpen = () => {
+    console.log("OPEN_GAME");
+    if ("undefined" !== typeof window.mplAndroidView)
+      try {
+        console.log("Inside Try Block");
+        window.mplAndroidView.performAction("OPEN_GAME");
+      } catch (er: any) {
+        console.log("anroid-error");
+        console.log(er.message);
+      }
   };
 
   const handleClose = () => {
-    try {
-      mplAndroidView.performAction("CLOSE");
-    } catch (er: any) {
-      console.log("anroid-error");
-      console.log(er.message);
-    }
+    console.log("CLOSE");
+    if ("undefined" !== typeof window.mplAndroidView)
+      try {
+        console.log("Inside Try Block");
+        window.mplAndroidView.performAction("CLOSE");
+      } catch (er: any) {
+        console.log("anroid-error");
+        console.log(er.message);
+      }
   };
 
   return (
-    <Card sx={{ width: 261, height: 172, margin: 0.5 }}>
+    <Card sx={{ width: 350, height: 300, margin: 0.5 }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           lorem
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit
-        </Typography>
+        <Canvas className="canvas">
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+        </Canvas>
       </CardContent>
       <CardActions sx={{ justifyContent: "center" }}>
         <Button size="small" onClick={handleClose}>
@@ -41,6 +79,9 @@ export default function MediaCard() {
         </Button>
         <Button size="small" onClick={handleOpen}>
           Open Game
+        </Button>
+        <Button size="small" onClick={handleOpento}>
+          Open Game2
         </Button>
       </CardActions>
     </Card>
